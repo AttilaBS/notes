7f9e4a4519d366c7fc0761aaacb37c0bb96d0494
AttilaBS <attila.barcellos.sipos@gmail.com>
2022-11-12 20:44:00 -0300

    scss with vite, welcome adjusts, others

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(
+        [
+            'auth:sanctum', config('jetstream.auth_session'),
+            'verified',
+        ]
+    )->group(function () {
+        Route::prefix('notes')->group(function () {
+            Route::get('/dashboard', function () {
+                return view('dashboard');
+            })->name('dashboard');
+            Route::get('/', function () {
+                return view('notes/index');
+            })->name('notes.index');
+        });
 });
