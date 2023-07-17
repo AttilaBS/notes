00fd01073fb3bc0198a14ebf7a34c6806e5a7546
Attila Barcellos Sipos <attila.barcellos.sipos@gmail.com>
2023-07-17 12:16:43 -0300

    welcome page layout improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,26 +16,26 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 py-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 sm:block login-box">
-                    @auth
-                        <a href="{{ route('notes.index') }}" class="text-bg text-gray-700 dark:text-gray-500 underline">Notas</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-bg text-gray-700 dark:text-gray-500 underline">Log in</a>
+                <div class="flex flex-align-center background">
+                    <div class="hidden fixed top-0 right-0 sm:block login-box">
+                        @auth
+                            <a href="{{ route('notes.index') }}" class="text-bg text-gray-700 dark:text-gray-500 underline">Notas</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-bg text-gray-700 dark:text-gray-500 underline">Logar</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-bg text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth{{--  --}}
-                </div>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-bg text-gray-700 dark:text-gray-500 underline">Cadastrar</a>
+                            @endif
+                        @endauth{{--  --}}
+                    </div>
             @endif
-            <div class="flex flex-align-center background">
-                <div class="flex column">
-                    <div class="title">
+                <div class="flex column welcome-container">
+                    <div class="welcome-title">
                         <h1>Bem-vindo ao Notes</h1>
                         <h3>Favor logar ou se registrar para usar a aplicação</h3>
                     </div>
                     <div class="flex logo-home">
-                        {{ svg('phosphor-note-pencil-duotone') }}
+                    <img src="{{ url('images/notebooks.webp') }}">
                     </div>
                 </div>
             </div>
